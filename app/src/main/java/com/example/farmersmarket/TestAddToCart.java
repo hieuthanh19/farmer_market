3a74e8dd48d0d82196b41ea6f2d26c94b6a330ef
jose <57227710+unrickq@users.noreply.github.com>
2020-11-12 16:55:20 +0700

    Add to cart

---
+++ app/src/main/java/com/example/farmersmarket/TestAddToCart.java
@@ -0,0 +1,73 @@
+package com.example.farmersmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.farmersmarket.database.AppDatabase;
+import com.example.farmersmarket.object.OrderDetail;
+import com.example.farmersmarket.object.Orders;
+
+import java.util.List;
+
+public class TestAddToCart extends AppCompatActivity {
+    public AppDatabase appDatabase;
+    Button addTocart;
+    Button goToCart;
+    List<OrderDetail> arrCart;
+    List<OrderDetail> arrCartByProductID;
+    List<Orders> arrOrder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_add_to_cart);
+        appDatabase = AppDatabase.getAppDatabase(this);
+        addTocart = findViewById(R.id.addToCart);
+        goToCart = findViewById(R.id.goToCart);
+
+        int accountID =1 ; //Bien thay doi
+        int productID =1 ; //Bien thay doi
+        double price = appDatabase.orderDetail().getProductPriceByProductID(productID);
+
+        arrCart = appDatabase.orderDetail().getAllCartInOrder();
+
+
+
+        addTocart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (arrCart.size()!=0){
+                    double productPrice;
+                    int ordersID = arrCart.get(0).ordersID;
+                    System.out.println("Order ID "+ ordersID);
+                    arrCartByProductID = appDatabase.orderDetail().getOrderDetailByOrderIDAndProductID(productID,ordersID);
+                    if (!arrCartByProductID.isEmpty()){
+                        int quantity = arrCartByProductID.get(0).quantity+1;
+                        productPrice = appDatabase.orderDetail().getProductPriceByProductID(productID);
+                        appDatabase.orderDetail().updateQuantityAndPrice(productID,ordersID,quantity,quantity*productPrice);
+                    }else{
+                        //productPrice = appDatabase.orderDetail().getProductPriceByProductID(productID);
+                        //appDatabase.orderDetail().insertOrderDetail(new OrderDetail(ordersID, productID, 1, productPrice, "", 1));
+                    }
+                }else{
+                    //appDatabase.ordersDAO().insertOrder(new Orders(1, accountID, 1, null, null, "1233123", 123, "No description", 0));
+                    //arrOrder = appDatabase.ordersDAO().getCurrentOrder();
+                    //appDatabase.orderDetail().insertOrderDetail(new OrderDetail(arrOrder.get(0).orderID, productID, 1, price, "", 1));
+                }
+            }
+        });
+
+        goToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), Cart.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
